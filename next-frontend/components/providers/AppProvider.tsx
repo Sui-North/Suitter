1e87c48765c444cd01c0d604e825152afbe94709
michaelperryjnr <michaelperryt97@gmail.com>
2025-11-15 09:20:06 +0000

    Added providers

---
+++ next-frontend/components/providers/AppProvider.tsx
@@ -0,0 +1,22 @@
+import {
+  SuiClientProvider,
+  WalletProvider,
+  createNetworkConfig,
+} from "@mysten/dapp-kit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const { networkConfig } = createNetworkConfig({
+  testnet: { url: "https://fullnode.testnet.sui.io:443" },
+});
+
+export function AppProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider autoConnect>{children}</WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
+}
